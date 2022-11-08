0aa7efaae2523518664ad78df9824b13ff3038df
DongjinSun <mystary72@gmail.com>
2022-11-08 23:46:11 +0000

    version 0.2

--- Daebak/employee/views.py
+++ Daebak/employee/views.py
@@ -1,8 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import User
+from django.http import HttpResponse
+from .module import *
+from .datacontrol import *
 
-# Create your views here.
-"""
-def home(request):
-    chat = 'Hello'
-    return render(request, 'em_stock.html', {'user_chat': chat})
-"""
+# EMPLOYEE
+# 메인
+def emmainpage(request):
+    return render(request, 'employeemain.html')
+
+# 회원가입
+def emsignuppage(request):
+    return render(request, 'em_signup.html')
+
+
+def emsignup(request):
+    if request.method == 'POST':
+        user = User()
+        user.name = request.POST['name']
+        user.phone = request.POST['phonenumber']
+        user.password = request.POST['password']
+        user.address = request.POST['address']   # 모델에 추가해야함.
+        user.card = request.POST['card']  # 모델에 추가해야 함.
+        user.save()
+
+    return redirect('em')
+
+# 로그인
+def emloginpage(request):
+    return render(request, 'login.html')
+
+def emlogin(request):
+    if request.method == 'POST':
+        login = Login_main()
+        
+        try:
+            login._user_login_init(int(request.POST['phonenumber']),request.POST['password'])
+        except:
+            pass
+
+    return redirect('uolp')
--- Daebak/user/views.py
+++ Daebak/user/views.py
@@ -4,24 +4,10 @@ from django.http import HttpResponse
 from .module import *
 from .datacontrol import *
 
+# USER
 # 메인
-def cusmain(request):
+def cusmainpage(request):
     return render(request, 'customermain.html')
-    
-# 로그인
-def loginpage(request):
-    return render(request, 'login.html')
-
-def login(request):
-    if request.method == 'POST':
-        login = Login_main()
-        
-        try:
-            login._user_login_init(int(request.POST['phonenumber']),request.POST['password'])
-        except:
-            pass
-
-    return redirect('lp')
 
 # 회원가입
 def signuppage(request):
@@ -37,8 +23,44 @@ def signup(request):
         user.card = request.POST['card']  # 모델에 추가해야 함.
         user.save()
 
-    return redirect('sp')
+    return redirect('cm')
 
+# 로그인
+def loginpage(request):
+    return render(request, 'login.html')
+
+def login(request):
+    if request.method == 'POST':
+        login = Login_main()
+        
+        try:
+            login._user_login_init(int(request.POST['phonenumber']),request.POST['password'])
+        except:
+            pass
+
+    return redirect('uolp')
+
+# 이전 주문내역 불러오기
+def userorderlistpage(request):
+    return render(request, 'userorderlist.html')
+
+def index(request):
+    user = User.objects.all()
+    context = {'user':user}
+
+    return render(request, 'userorderlist.html', context)
+
+# 디너종류 고르기
+def dfpage(request):
+    return render(request, 'dinnerfood.html')
+
+# 디너스타일 고르기
+def dspage(request):
+    return render(request, 'dinnerstyle.html')
+
+# 추가사항 고르기
+def addpage(request):
+    return render(request, 'addition.html')
 
 # 주문
 def orderpage(request):
@@ -56,13 +78,5 @@ def order(request):
         user.save()
 
     return redirect('op')
-
-
-# 이전 주문내역 불러오기
-def index(request):
-    user = User.objects.all()
-    context = {'user':user}
-
-    return render(request, 'userorderlist.html', context)
 
 
